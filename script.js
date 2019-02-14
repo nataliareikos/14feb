428bbc9f97ffa94f4471b98b336b45f53682ba5a
Natália Reiko Sakamoto <nataliareikosakamoto@gmail.com>
2019-02-14 14:51:20 -0200

    exercicio hamburguer

--- script.js
+++ script.js
@@ -19,10 +19,10 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 const navMenuHamburguer = document.querySelector('.nav-menu-hamburguer');
 
-const atualizarMenu = function () {
-    navMenuHamburguer.classList.toggle('ativo');
+const visualizarMenu = function () {
+    menuHamburguer.classList.toggle('visivel');
     navMenuHamburguer.classList.toggle('visivel');
 
 }
 
-menuHamburguer.onclick = atualizarMenu;
+menuHamburguer.onclick = visualizarMenu;
